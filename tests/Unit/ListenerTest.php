94a75586d1d6ef18c8bcd8f75fd715f1d6df4443
begimov <begimov@gmail.com>
2017-09-24 17:53:30 +0300

    handle method accepts type-hinted event based class

--- tests/Unit/ListenerTest.php
+++ tests/Unit/ListenerTest.php
@@ -4,16 +4,26 @@ namespace App\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 use App\Tests\Stubs\ListenerStub;
+use App\Tests\Stubs\EventStub;
 
 class ListenerTest extends TestCase
 {
     /** @test */
-    public function handle_throws_error_when_invalid_event_is_passed_in()
+    public function handle_accepts_type_hinted_event()
     {
-        $this->expectException(InvalidArgumentException::class);
+        // $this->expectException(\TypeError::class);
 
         $listener = new ListenerStub();
-        $listener->handle('pseudo_event');
+        $listener->handle(new EventStub());
     }
+
+    /** @test */
+    // public function handle_throws_error_when_invalid_event_is_passed_in()
+    // {
+    //     // $this->expectException(\TypeError::class);
+    //
+    //     $listener = new ListenerStub();
+    //     $listener->handle(new EventStub());
+    // }
 
 }
