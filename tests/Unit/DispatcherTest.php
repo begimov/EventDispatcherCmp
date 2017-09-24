b24a61f5a1381a704732d43a2389b070d36b5730
begimov <begimov@gmail.com>
2017-09-24 18:03:25 +0300

    dispatcher test added

---
+++ tests/Unit/DispatcherTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class DispatcherTest extends TestCase
+{
+    /** @test */
+    public function can_get_event_name()
+    {
+        //
+    }
+
+}
