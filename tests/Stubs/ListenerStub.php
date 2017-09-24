b3cba4e26ac47d05eeed849015d86a549ec1db71
begimov <begimov@gmail.com>
2017-09-24 17:57:22 +0300

    base event listener class created and stub extended from it

--- tests/Stubs/ListenerStub.php
+++ tests/Stubs/ListenerStub.php
@@ -2,9 +2,10 @@
 
 namespace App\Tests\Stubs;
 
+use App\Core\Listeners\Listener;
 use App\Core\Events\Event;
 
-class ListenerStub
+class ListenerStub extends Listener
 {
     public function handle(Event $event)
     {
