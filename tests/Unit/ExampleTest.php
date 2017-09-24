0b63b86e5028bfca70f147225ae2a8ae0a619a05
begimov <begimov@gmail.com>
2017-09-24 16:26:05 +0300

    example phpunit test created

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function example()
+    {
+        $this->assertTrue(true, true);
+    }
+}
